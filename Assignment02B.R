8d47f3e6c204e768f78143e62c5b6eab71785eb1
Jack DiPietro <jsd324@lehigh.edu>
2020-09-16 22:12:41 -0400

    Assignment
    02B.R

---
+++ Assignment02B.R
@@ -0,0 +1,28 @@
+# Data Frames
+x <- 10:1
+y <- -4:5
+z <- c('Hockey', 'Football', 'Curling', 'Soccer', 'Rugby', 'Baseball', 'Golf', 'Basketball', 'Wrestling', 'Tennis')
+theDF <- data.frame(x,y,z)
+theDF
+
+str(theDF)
+
+theDF[1,2]
+View(theDF)
+
+# Using names
+theDF$x
+theDF$x[3]
+
+# Editing names
+names(theDF)<-c("Popularity", "Team Strength", "Sport")
+View(theDF)
+
+# Summary of referencing data frame elements
+theDF[2:3]
+theDF[c(1,3)]
+theDF[3]
+theDF$Sport
+theDF["Sport"]
+
+sum(theDF["Popularity"])
